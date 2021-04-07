207046d18edc76ab0006fd3211428a47f340196f
a70n3 <prohibitous@gmail.com>
2021-04-07 15:29:45 +0800

    add sample

---
+++ Sample/Sample/PagedResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static Sample.Pages.FetchData;
+
+namespace Sample
+{
+    public class PagedResult
+    {
+        public int NumberOfPages => (int)Math.Ceiling((decimal)TotalRecords / RecordPerPage);
+        public int RecordPerPage { get; set; }
+        public int TotalRecords { get; set; }
+        public WeatherForecast[] ResultSet { get; set; } = Array.Empty<WeatherForecast>();
+    }
+}
